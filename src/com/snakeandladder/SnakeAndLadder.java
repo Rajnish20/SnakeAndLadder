c62e9b57beed4588d5c17d21c9dad714f0ad682a
Rajnish Sahu <rajnishsahu95@gmail.com>
2021-05-20 23:23:20 +0530

    Added Second player and changed logic

--- src/com/snakeandladder/SnakeAndLadder.java
+++ src/com/snakeandladder/SnakeAndLadder.java
@@ -1,37 +1,60 @@
 package com.snakeandladder;
 
 public class SnakeAndLadder {
-	public static int position = 0;
-	public static int count = 0;
+	public static int player1Position = 0;
+	public static int player2Position = 0;
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to Snake and Ladder");
-		System.out.println(position);
+		while (player1Position != 100 && player2Position != 100) {
 
-		while (position != 100) {
-			count++;
-			int nextPosition = (int) (Math.random() * 6) + 1;
-			int decision = (int) (Math.random() * 3);
-			switch (decision) {
-			case 1:
-				position += nextPosition;
-				break;
-			case 2:
-				position -= nextPosition;
-				if (position < 0)
-					position = 0;
-				break;
-			default:
-				position += 0;
-
-			}
-			if (position > 100) {
-				position -= nextPosition;
-			}
-			System.out.println(position);
+			int player1Turn = play(player1Position);
+			player1Position = player1Turn;
+			int player2Turn = play(player2Position);
+			player2Position = player2Turn;
 
 		}
-		System.out.println("You win after : " + count);
+		if (player1Position == 100)
+			System.out.println("Player1 wins");
+		else
+			System.out.println("Player2 wins");
 
+	}
+
+	public static int play(int player_position) {
+		int options = (int) (Math.random() * 3);
+		int dice = (int) (Math.random() * 6) + 1;
+		int nextPosition = player_position;
+		switch (options) {
+		case 1:
+			nextPosition += dice;
+			if (nextPosition > 100) {
+				nextPosition -= dice;
+			}
+			while (options == 1) {
+				dice = (int) (Math.random() * 6) + 1;
+				options = (int) (Math.random() * 3);
+				nextPosition += dice;
+				if (nextPosition > 100)
+					break;
+			}
+			if (options == 2) {
+				nextPosition -= dice;
+				if (nextPosition < 0) {
+					nextPosition = 0;
+				}
+			}
+			break;
+		case 2:
+			nextPosition -= dice;
+			if (nextPosition < 0) {
+				nextPosition = 0;
+			}
+			break;
+		default:
+			nextPosition += 0;
+
+		}
+		return nextPosition;
 	}
 }
