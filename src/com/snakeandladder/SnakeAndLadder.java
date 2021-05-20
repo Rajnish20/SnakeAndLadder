c9181cd32a64d72f971b24161e3423bbc22a7af9
Rajnish Sahu <rajnishsahu95@gmail.com>
2021-05-20 23:20:20 +0530

    Added logic for counting total dice rolls

--- src/com/snakeandladder/SnakeAndLadder.java
+++ src/com/snakeandladder/SnakeAndLadder.java
@@ -2,19 +2,19 @@ package com.snakeandladder;
 
 public class SnakeAndLadder {
 	public static int position = 0;
+	public static int count = 0;
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to Snake and Ladder");
 		System.out.println(position);
 
 		while (position != 100) {
+			count++;
 			int nextPosition = (int) (Math.random() * 6) + 1;
 			int decision = (int) (Math.random() * 3);
 			switch (decision) {
 			case 1:
 				position += nextPosition;
-				if (position > 100)
-					position -= nextPosition;
 				break;
 			case 2:
 				position -= nextPosition;
@@ -25,8 +25,13 @@ public class SnakeAndLadder {
 				position += 0;
 
 			}
+			if (position > 100) {
+				position -= nextPosition;
+			}
+			System.out.println(position);
 
 		}
+		System.out.println("You win after : " + count);
 
 	}
 }
