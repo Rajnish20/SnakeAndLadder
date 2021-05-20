1db39e922086ed93434fc50852edb0b935475628
Rajnish Sahu <rajnishsahu95@gmail.com>
2021-05-20 23:08:02 +0530

    Added start position

--- src/com/snakeandladder/SnakeAndLadder.java
+++ src/com/snakeandladder/SnakeAndLadder.java
@@ -1,9 +1,8 @@
 package com.snakeandladder;
 
 public class SnakeAndLadder {
-
 	public static void main(String[] args) {
 		System.out.println("Welcome to Snake and Ladder");
-
+		int position = 0;
 	}
 }
