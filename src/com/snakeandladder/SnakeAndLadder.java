76672178a0bb919ca3d7b01b5a7d98db67b48c4b
Rajnish Sahu <rajnishsahu95@gmail.com>
2021-05-20 23:14:03 +0530

    Added options to play

--- src/com/snakeandladder/SnakeAndLadder.java
+++ src/com/snakeandladder/SnakeAndLadder.java
@@ -1,12 +1,27 @@
-
 package com.snakeandladder;
 
 public class SnakeAndLadder {
+	public static int position = 0;
+
 	public static void main(String[] args) {
 		System.out.println("Welcome to Snake and Ladder");
-		int position  = 0;
-		
-		int nextPosition = (int) (Math.random() * 6) + 1 ;
-		
+
+		int nextPosition = (int) (Math.random() * 6) + 1;
+
+		int decision = (int) (Math.random() * 3);
+		switch (decision) {
+		case 1:
+			position += nextPosition;
+			break;
+		case 2:
+			position -= nextPosition;
+			if (position < 0)
+				position = 0;
+			break;
+		default:
+			position += 0;
+
+		}
+
 	}
 }
